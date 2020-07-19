5e9639abe8b247b4b8e90ba6d2537242e02a3e98
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-19 23:17:20 +0700

    Zvjz

--- function.php
+++ function.php
@@ -12,6 +12,7 @@ $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
 $header[] = "X-Location: -7.38".mt_rand(1000,9999).",112.73".mt_rand(1000,9999)
+if ($pin):
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
